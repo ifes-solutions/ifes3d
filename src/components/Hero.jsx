3823066a70aa991da952da6b596611e24d7514ba
ifes-solutions <ifes@ifes-solutions.com>
2023-04-28 17:41:53 +0800

    Project Finised

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,11 +15,10 @@ const Hero = () => {
                 </div>
                 <div>
                     <h1 className={`${styles.heroHeadText} text-white`}>
-                        {" "}
                         Hi, I'm <span className="text-[#915eff]">IFES</span>
                     </h1>
                     <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-                        I develop 3D virusals, user{" "}
+                        I develop 3D visuals, user
                         <br className="sm:block hidden" /> interfaces and web
                         applications
                     </p>
